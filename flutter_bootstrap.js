290426d2072f30f31f297de33f24cf6c19116dad
krutika thakkar <thakkarkrutika4@gmail.com>
2024-11-27 19:54:43 +0530

    Resolved conflict in assets/NOTICES and completed merge

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,7 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-
-    serviceWorkerVersion: "2887444218"
+    serviceWorkerVersion: "4026640528"
   }
 });
