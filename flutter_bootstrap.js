1203c28ff803ba534edc1ed503e14328a337eb4d
krutika thakkar <thakkarkrutika4@gmail.com>
2024-11-27 18:20:02 +0530

    updated web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3386673591"
+    serviceWorkerVersion: "1676770373"
   }
 });
