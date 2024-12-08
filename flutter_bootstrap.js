faa61f7d966f30111eec452e8c03f90448b03cdf
krutika thakkar <thakkarkrutika4@gmail.com>
2024-12-08 10:31:40 +0530

    some changes for mobile version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3583162115"
+    serviceWorkerVersion: "3597040448"
   }
 });
